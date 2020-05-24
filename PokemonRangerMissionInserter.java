58e4fd1bf75f3976b7fbe8fdf56cde5aa48d7fc1
Chris-G-Bowles <chris.g.bowles@gmail.com>
2020-05-23 23:05:02 -0700

    Reworked first error case.

--- PokemonRangerMissionInserter.java
+++ PokemonRangerMissionInserter.java
@@ -8,105 +8,104 @@ import java.util.Scanner;
 public class PokemonRangerMissionInserter {
 	public static void main(String[] args) {
 		System.out.println("* Pokemon Ranger Mission Inserter *");
-		if (args.length == 0 || args.length == 3) {
-			Scanner input = new Scanner(System.in);
-			String inputSaveFileLocation;
-			if (args.length == 0) {
-				System.out.print("Enter the input save file's location: ");
-				inputSaveFileLocation = input.nextLine();
-			} else {
-				inputSaveFileLocation = args[0];
-			}
-			String gameOption;
-			if (args.length == 0) {
-				System.out.println("Select the save file's game:");
-				System.out.println("1) Pokemon Ranger: Shadows of Almia");
-				System.out.println("2) Pokemon Ranger: Guardian Signs");
-				System.out.print("Game option: ");
-				gameOption = input.nextLine();
-			} else {
-				gameOption = args[1];
-			}
-			String outputSaveFileLocation;
-			if (args.length == 0) {
-				System.out.print("Enter the output save file's location: ");
-				outputSaveFileLocation = input.nextLine();
-			} else {
-				outputSaveFileLocation = args[2];
-			}
-			input.close();
-			try {
-				FileInputStream inputStream = new FileInputStream(inputSaveFileLocation);
-				int token;
-				ArrayList<Integer> inputSaveFile = new ArrayList<>();
-				do {
+		if (args.length != 0 && args.length != 3) {
+			error("This program's usage is as follows:\n" +
+					"java PokemonRangerMissionInserter\n" +
+					"java PokemonRangerMissionInserter <input save file location> <game option> " +
+					"<output save file location>");
+		}
+		Scanner input = new Scanner(System.in);
+		String inputSaveFileLocation;
+		if (args.length == 0) {
+			System.out.print("Enter the input save file's location: ");
+			inputSaveFileLocation = input.nextLine();
+		} else {
+			inputSaveFileLocation = args[0];
+		}
+		String gameOption;
+		if (args.length == 0) {
+			System.out.println("Select the save file's game:");
+			System.out.println("1) Pokemon Ranger: Shadows of Almia");
+			System.out.println("2) Pokemon Ranger: Guardian Signs");
+			System.out.print("Game option: ");
+			gameOption = input.nextLine();
+		} else {
+			gameOption = args[1];
+		}
+		String outputSaveFileLocation;
+		if (args.length == 0) {
+			System.out.print("Enter the output save file's location: ");
+			outputSaveFileLocation = input.nextLine();
+		} else {
+			outputSaveFileLocation = args[2];
+		}
+		input.close();
+		try {
+			FileInputStream inputStream = new FileInputStream(inputSaveFileLocation);
+			int token;
+			ArrayList<Integer> inputSaveFile = new ArrayList<>();
+			do {
+				try {
+					token = inputStream.read();
+				} catch (Exception e) {
+					token = -1;
+				}
+				if (token >= 0 && token <= 255) {
+					inputSaveFile.add(token);
+				}
+			} while (token >= 0 && token <= 255);
+			inputStream.close();
+			int inputSaveFileSize = 262144;
+			if (inputSaveFile.size() == inputSaveFileSize) {
+				if (isValidInteger(gameOption) &&
+						(Integer.parseInt(gameOption) == 1 || Integer.parseInt(gameOption) == 2)) {
+					int gameIndex = Integer.parseInt(gameOption) - 1;
+					String[] insertionFileNames = {"RangerNetMissions2.bin", "RangerNetMissions3.bin"};
 					try {
-						token = inputStream.read();
-					} catch (Exception e) {
-						token = -1;
-					}
-					if (token >= 0 && token <= 255) {
-						inputSaveFile.add(token);
-					}
-				} while (token >= 0 && token <= 255);
-				inputStream.close();
-				int inputSaveFileSize = 262144;
-				if (inputSaveFile.size() == inputSaveFileSize) {
-					if (isValidInteger(gameOption) &&
-							(Integer.parseInt(gameOption) == 1 || Integer.parseInt(gameOption) == 2)) {
-						int gameIndex = Integer.parseInt(gameOption) - 1;
-						String[] insertionFileNames = {"RangerNetMissions2.bin", "RangerNetMissions3.bin"};
-						try {
-							inputStream = new FileInputStream(insertionFileNames[gameIndex]);
-							ArrayList<Integer> insertionFile = new ArrayList<>();
-							do {
-								try {
-									token = inputStream.read();
-								} catch (Exception e) {
-									token = -1;
-								}
-								if (token >= 0 && token <= 255) {
-									insertionFile.add(token);
-								}
-							} while (token >= 0 && token <= 255);
-							inputStream.close();
-							int[] insertionFileSizes = {148034, 153344};
-							if (insertionFile.size() == insertionFileSizes[gameIndex]) {
-								try {
-									FileOutputStream outputStream = new FileOutputStream(outputSaveFileLocation);
-									for (int i = 0; i < inputSaveFile.size() - insertionFile.size(); i++) {
-										outputStream.write(inputSaveFile.get(i));
-									}
-									for (int i = 0; i < insertionFile.size(); i++) {
-										outputStream.write(insertionFile.get(i));
-									}
-									outputStream.close();
-									System.out.println("Success: " + outputSaveFileLocation + " was created!");
-								} catch (Exception e) {
-									System.out.println("Error: Could not create " + outputSaveFileLocation + ".");
-								}
-							} else {
-								System.out.println("Error: " + insertionFileNames[gameIndex] + " is not " +
-										insertionFileSizes[gameIndex] + " bytes in size.");
+						inputStream = new FileInputStream(insertionFileNames[gameIndex]);
+						ArrayList<Integer> insertionFile = new ArrayList<>();
+						do {
+							try {
+								token = inputStream.read();
+							} catch (Exception e) {
+								token = -1;
 							}
-						} catch (Exception e) {
-							System.out.println("Error: " + insertionFileNames[gameIndex] + " not found.");
+							if (token >= 0 && token <= 255) {
+								insertionFile.add(token);
+							}
+						} while (token >= 0 && token <= 255);
+						inputStream.close();
+						int[] insertionFileSizes = {148034, 153344};
+						if (insertionFile.size() == insertionFileSizes[gameIndex]) {
+							try {
+								FileOutputStream outputStream = new FileOutputStream(outputSaveFileLocation);
+								for (int i = 0; i < inputSaveFile.size() - insertionFile.size(); i++) {
+									outputStream.write(inputSaveFile.get(i));
+								}
+								for (int i = 0; i < insertionFile.size(); i++) {
+									outputStream.write(insertionFile.get(i));
+								}
+								outputStream.close();
+								System.out.println("Success: " + outputSaveFileLocation + " was created!");
+							} catch (Exception e) {
+								System.out.println("Error: Could not create " + outputSaveFileLocation + ".");
+							}
+						} else {
+							System.out.println("Error: " + insertionFileNames[gameIndex] + " is not " +
+									insertionFileSizes[gameIndex] + " bytes in size.");
 						}
-					} else {
-						System.out.println("Error: Invalid game option.");
+					} catch (Exception e) {
+						System.out.println("Error: " + insertionFileNames[gameIndex] + " not found.");
 					}
 				} else {
-					System.out.println("Error: " + inputSaveFileLocation + " is not " + inputSaveFileSize +
-							" bytes in size.");
+					System.out.println("Error: Invalid game option.");
 				}
-			} catch (Exception e) {
-				System.out.println("Error: " + inputSaveFileLocation + " not found.");
+			} else {
+				System.out.println("Error: " + inputSaveFileLocation + " is not " + inputSaveFileSize +
+						" bytes in size.");
 			}
-		} else {
-			System.out.println("This program's usage is as follows:");
-			System.out.println("java PokemonRangerMissionInserter");
-			System.out.println("java PokemonRangerMissionInserter <input save file location> <game option> " +
-					"<output save file location>");
+		} catch (Exception e) {
+			System.out.println("Error: " + inputSaveFileLocation + " not found.");
 		}
 	}
 	
